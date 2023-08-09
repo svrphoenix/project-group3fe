fca3fc0a2069b60ec041c1d25ef67aa072706fed
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-08-09 20:03:46 +0100

    added routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,74 @@
-import { Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
+import MainLayout from 'pages/MainLayout/MainLayout';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
+
+const MainPage = lazy(() => import('../pages/MainPage/MainPage'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
+const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
+const CalendarPage = lazy(() => import('../pages/CalendarPage/CalendarPage'));
+const AccountPage = lazy(() => import('../pages/AccountPage/AccountPage'));
+const StatisticsPage = lazy(() =>
+  import('../pages/StatisticsPage/StatisticsPage')
+);
+const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
 
 const App = () => {
-  <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<MainPage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/calendar"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/calendar" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/calendar"
+          element={
+            <PrivateRoute redirectTo="/login" component={<CalendarPage />} />
+          }
+        >
+          <Route
+            path="month/:currentDate"
+            element={
+              <PrivateRoute
+                redirectTo="/login"
+                component={<div>ChoosedMonth</div>}
+              />
+            }
+          />
+        </Route>
+        <Route
+          path="/account"
+          element={
+            <PrivateRoute redirectTo="/login" component={<AccountPage />} />
+          }
+        />
+        <Route
+          path="/statistics"
+          element={
+            <PrivateRoute redirectTo="/login" component={<StatisticsPage />} />
+          }
+        />
+        <Route
+          path="*"
+          element={<RestrictedRoute redirectTo="/" component={<NotFound />} />}
+        />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
--- src/pages/MainLayout/MainLayout.jsx
+++ src/pages/MainLayout/MainLayout.jsx
@@ -1,7 +1,15 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const MainLayout = () => {
-  return <></>;
+  return (
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+    </>
+  );
 };
 
 MainLayout.propTypes = {};
