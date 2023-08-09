53fedbf9f69819d42c9c338ab3eaedafb72db099
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-08-09 21:28:00 +0100

    changed isRefreshing variable

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -7,12 +7,12 @@ import {
 
 export const useAuth = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  const isRefreshing = useSelector(selectIsRefreshing);
+  const isLoading = useSelector(selectIsLoading);
   const user = useSelector(selectUser);
 
   return {
     isLoggedIn,
-    isRefreshing,
+    isLoading,
     user,
   };
 };
