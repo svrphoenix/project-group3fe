21ca7e90d1d151181f54ece7344483a5bab64d5e
Crash-afftar <sashakhymenko88@gmail.com>
2023-08-11 00:46:40 +0300

    preparation 1108

---
+++ src/pages/CalendarPage/TaskForm/TaskForm.jsx
@@ -0,0 +1,3 @@
+const TaskForm = () => { };
+
+export default TaskForm;
---
+++ src/pages/CalendarPage/TaskModal/TaskModal.jsx
@@ -0,0 +1,18 @@
+import { useDispatch } from "react-redux";
+import TaskForm from "../TaskForm/TaskForm";
+
+// import Modal
+
+
+
+const TaskModal = () => {
+    const dispatch = useDispatch();
+    const closeModal = () => { };
+
+    return (
+        // <Modal onClose={closeModal}>
+        <TaskForm closeModal={closeModal } {...props} />
+
+        // </Modal>
+    );
+};
