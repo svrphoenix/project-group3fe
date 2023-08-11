093edcc11f4817f33035d00e331df124a3db0039
Zhenya09 <klevets00@gmail.com>
2023-08-12 01:38:59 +0300

    add

--- src/pages/NotFound/NotFound.styled.js
+++ src/pages/NotFound/NotFound.styled.js
@@ -5,15 +5,32 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  width: 1440px;
+  height: 900px;
+  background: #f7f6f9;
+
+  @media (max-width: 768px) {
+    width: 768px;
+    height: 1024px;
+  }
+
+  @media (max-width: 375px) {
+    width: 375px;
+    height: 812px;
+  }
 `;
 
 export const Heading = styled.h1`
-  font-size: 1.5rem;
-  margin-bottom: 1rem;
+  color: rgba(17, 17, 17, 0.7);
+  text-align: center;
+  font-family: Inter;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px;
 `;
 
-export const ErrorImagesContainer = styled.div`
+export const div = styled.div`
   color: #3e85f3;
   text-shadow: 0px 9.399999618530273px 57.6875px rgba(0, 0, 0, 0.04),
     0px 47px 355px rgba(0, 0, 0, 0.07);
@@ -21,17 +38,51 @@ export const ErrorImagesContainer = styled.div`
   font-size: 200px;
   font-style: normal;
   font-weight: 700;
-  line-height: 250px; /* 125% */
-`;
-
-export const ErrorImage = styled.img`
-  width: 300px;
-  margin-bottom: 1rem;
+  line-height: 250px;
 
   @media (max-width: 768px) {
+    color: #3e85f3;
+    text-shadow: 0px 9.399999618530273px 57.6875px rgba(0, 0, 0, 0.04),
+      0px 47px 355px rgba(0, 0, 0, 0.07);
+    font-family: Inter;
+    font-size: 200px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 250px;
   }
 
   @media (max-width: 375px) {
+    color: #3e85f3;
+    text-shadow: 0px 9.399999618530273px 57.6875px rgba(0, 0, 0, 0.04),
+      0px 47px 355px rgba(0, 0, 0, 0.07);
+    font-family: Inter;
+    font-size: 100px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 150px;
+  }
+`;
+
+export const StyledErrorImage = styled.img`
+  width: 178px;
+  height: 414px;
+  transform: rotate(-31deg);
+  flex-shrink: 0;
+
+  @media (max-width: 768px);
+   {
+    width: 178px;
+    height: 414px;
+    transform: rotate(-31.558deg);
+    flex-shrink: 0;
+  }
+
+  @media (max-width: 375px);
+   {
+    width: 81px;
+    height: 188px;
+    transform: rotate(-31deg);
+    flex-shrink: 0;
   }
 `;
 
@@ -44,4 +95,30 @@ export const Button = styled.button`
   border-radius: 16px;
   background: #3e85f3;
   box-shadow: 4px 2px 16px 0px rgba(136, 165, 191, 0.48);
+
+  @media (max-width: 768px);
+   {
+    display: inline-flex;
+    padding: 16px 48px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+    border-radius: 16px;
+    background: #3e85f3;
+    box-shadow: 4px 2px 16px 0px rgba(136, 165, 191, 0.48);
+  }
+
+  @media (max-width: 375px);
+   {
+    display: inline-flex;
+    padding: 14px 32px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+    border-radius: 16px;
+    background: #3e85f3;
+    box-shadow: 4px 2px 16px 0px rgba(136, 165, 191, 0.48);
+  }
 `;
+
+export const ErrorImagesContainer = styled.div``;
