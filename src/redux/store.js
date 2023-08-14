db40cc44f237f1abebeec79a1aa787337407fa00
Crash-afftar <sashakhymenko88@gmail.com>
2023-08-14 23:23:36 +0300

    taskredux 14082023

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,6 +12,7 @@ import {
 } from 'redux-persist';
 
 import { authReducer } from './auth/slice';
+import { tasksReducer } from './tasks/slice';
 
 const authPersistConfig = {
   key: 'auth',
@@ -22,6 +23,7 @@ const authPersistConfig = {
 export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authReducer),
+    tasks: tasksReducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
