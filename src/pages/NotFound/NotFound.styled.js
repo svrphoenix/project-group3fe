a8c75f62519cb1de0779d76d33184e72a2ee348e
Zhenya09 <klevets00@gmail.com>
2023-08-15 14:55:04 +0300

    add batton

--- src/pages/NotFound/NotFound.styled.js
+++ src/pages/NotFound/NotFound.styled.js
@@ -23,18 +23,16 @@ export const Container = styled.div`
 export const Heading = styled.h1`
   color: rgba(17, 17, 17, 0.7);
   text-align: center;
-  font-family: Inter;
   font-size: 14px;
   font-style: normal;
   font-weight: 500;
   line-height: 18px;
 `;
 
-export const div = styled.div`
+export const Errortext = styled.div`
   color: #3e85f3;
   text-shadow: 0px 9.399999618530273px 57.6875px rgba(0, 0, 0, 0.04),
     0px 47px 355px rgba(0, 0, 0, 0.07);
-  font-family: Inter;
   font-size: 200px;
   font-style: normal;
   font-weight: 700;
@@ -95,6 +93,11 @@ export const Button = styled.button`
   border-radius: 16px;
   background: #3e85f3;
   box-shadow: 4px 2px 16px 0px rgba(136, 165, 191, 0.48);
+  color: #fff;
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 24px;
+  letter-spacing: -0.36px;
 
   @media (max-width: 768px);
    {
@@ -106,6 +109,12 @@ export const Button = styled.button`
     border-radius: 16px;
     background: #3e85f3;
     box-shadow: 4px 2px 16px 0px rgba(136, 165, 191, 0.48);
+    color: #fff;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 24px;
+    letter-spacing: -0.36px;
   }
 
   @media (max-width: 375px);
@@ -118,7 +127,15 @@ export const Button = styled.button`
     border-radius: 16px;
     background: #3e85f3;
     box-shadow: 4px 2px 16px 0px rgba(136, 165, 191, 0.48);
+    color: #fff;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 24px;
+    letter-spacing: -0.36px;
   }
 `;
 
 export const ErrorImagesContainer = styled.div``;
+
+export const textimg = styled.div``;
