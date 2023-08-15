186170cc01f6c3f40f878d5d8fc5a2b7efd34fe0
Serhii Rudenko <svr.phoenix@gmail.com>
2023-08-15 13:37:04 +0300

    Added Toaster in SharedLayout

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -1,6 +1,7 @@
 import useAuth from 'hooks/useAuth';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router';
+import { Toaster } from 'react-hot-toast';
 import { Loader } from './Loader/Loader';
 
 export const SharedLayout = () => {
@@ -11,6 +12,7 @@ export const SharedLayout = () => {
   ) : (
     <Suspense fallback={<Loader />}>
       <Outlet />
+      <Toaster position="top-right" reverseOrder={false} />
     </Suspense>
   );
 };
