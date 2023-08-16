44b52b5e603220b211244ab40b7f19394076537a
KuhaiAnna <kuhai.anna@gmail.com>
2023-08-16 19:55:34 +0300

    Change styles of Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,37 @@
+import { PropTypes } from 'prop-types';
 import { ColorRing } from 'react-loader-spinner';
+import { createPortal } from 'react-dom';
+
+const loaderRoot = document.querySelector('#loader-root');
 
 export const Loader = () => {
-  return (
-    <ColorRing
-      visible={true}
-      height="80"
-      width="80"
-      ariaLabel="blocks-loading"
-      wrapperStyle={{}}
-      wrapperClass="blocks-wrapper"
-      colors={['#3e85f3', '#FDF608', '#3e85f3', '#3e85f3', '#3e85f3']}
-    />
+  return createPortal(
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: '#ffffff6d',
+        backdropFilter: 'blur(1px)',
+      }}
+    >
+      <ColorRing
+        visible={true}
+        height="90"
+        width="90"
+        ariaLabel="blocks-loading"
+        colors={['#3e85f3', '#FDF608', '#3e85f3', '#3e85f3', '#3e85f3']}
+      />
+    </div>,
+    loaderRoot
   );
+};
+
+Loader.propTypes = {
+  size: PropTypes.string,
 };
