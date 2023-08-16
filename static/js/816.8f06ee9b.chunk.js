cb0a317fdacf7a9927a7469211f1a967832b769c
svrphoenix <svr.phoenix@gmail.com>
2023-08-16 17:34:31 +0000

    Deploying to gh-pages from @ svrphoenix/project-group3fe@20168d062edce1c71e9b6ddb1f7022d365c1bb0c 🚀

---
+++ static/js/816.8f06ee9b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkproject_group3fe=self.webpackChunkproject_group3fe||[]).push([[816],{1354:function(e,n,c){c.r(n);var r=c(1951),t=c(2791),a=c(7689),u=c(3329);n.default=function(){var e=(0,a.s0)();return(0,t.useEffect)((function(){!function(e){var n=(0,r.default)(new Date,"MMMM").toLowerCase();e("/calendar/month/".concat(n))}(e)}),[e]),(0,u.jsx)(u.Fragment,{children:(0,u.jsx)(t.Suspense,{fallback:(0,u.jsx)("div",{children:"Loading ...."}),children:(0,u.jsx)(a.j3,{})})})}}}]);
+//# sourceMappingURL=816.8f06ee9b.chunk.js.map
