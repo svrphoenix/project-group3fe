d03d3f7fc9d1450177e5a033b3457730c1e860d6
VictorLgchv <108681513+VictorLgchv@users.noreply.github.com>
2023-08-17 13:10:38 +0300

    Create variables.jsx

---
+++ src/utils/variables.jsx
@@ -0,0 +1,27 @@
+const variables = {
+  firstMainColor: '#13151A',
+  secondMainColor: '#3E85F3',
+  thirdMainColor: '#DCEBF7',
+  mainMenuIconColor: '#84828A',
+  activeMenuIconColor: '#3E85F3',
+  activeMenuItemColor: '#E3F3FF',
+  mainMenuTextColor: 'rgba(52, 52, 52, 0.5)',
+  firstStatisticsColor:
+    'linear-gradient(0deg, #FFD2DD 0%, rgba(255, 210, 221, 0) 96.87%)',
+  secondStatisticsColor:
+    'linear-gradient(0deg, #3E85F3 0%, rgba(62, 133, 243, 0) 100%)',
+  firstTextColor: '#111111',
+  secondTextColor: '#343434',
+  thirdTextColor: '#F7F6F9',
+  nameColor: '#343434',
+  firstStatusColor: '#72C2F8',
+  secondStatusColor: '#F3B249',
+  thirdStatusColor: '#EA3D65',
+  btnMainColor: '#3E85F3',
+  hoverBtnMAinColor: '#2B78EF',
+  firstBtntnTextColor: '#fff',
+  toogleIconColor: '#3E85F3',
+  cubic: '250ms cubic-bezier(0.4, 0, 0.2, 1)',
+};
+
+export default variables;
