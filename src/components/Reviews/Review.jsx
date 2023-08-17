e82b5e1c4e3fbe0baf8753c531f31a21431d06e3
X Mister <someinkognito@gmail.com>
2023-08-17 07:24:29 +0300

    add reviews from backend

--- src/components/Reviews/Review.jsx
+++ src/components/Reviews/Review.jsx
@@ -1,19 +1,18 @@
-import { IMG } from 'images';
 import * as SC from './Review.styled';
 import { StarRating } from 'components/StarRating/StarRating';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import PropTypes from 'prop-types';
 
-export const Review = ({
-  avatar = IMG.defaultAvatar,
-  rating,
-  name,
-  review,
-}) => {
+export const Review = ({ avatar, rating, name, review }) => {
   return (
     <>
       <SC.ReviewWrapper>
         <SC.ReviewWrapperUser>
-          <SC.ReviewAvatar src={avatar} alt={name} width="50" />
+          {avatar ? (
+            <SC.ReviewAvatar src={avatar} alt={name} width="50" />
+          ) : (
+            <AccountCircleIcon sx={{ width: 50, height: 50, color: '#aaa' }} />
+          )}
           <SC.ReviewWrapperGrade>
             <SC.ReviewUsername>{name}</SC.ReviewUsername>
             <StarRating rating={rating} readOnly />
