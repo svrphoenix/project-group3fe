9b0b1972003fc139641014beacaaaa39a701e63a
KuhaiAnna <kuhai.anna@gmail.com>
2023-08-17 16:37:15 +0300

    Remove Loader from components
    Remove Loader from App and SharedLayout

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -1,15 +1,10 @@
-import useAuth from 'hooks/useAuth';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router';
 import { Toaster } from 'react-hot-toast';
 import { Loader } from './Loader/Loader';
 
 export const SharedLayout = () => {
-  const { isLoading } = useAuth();
-
-  return isLoading ? (
-    <Loader />
-  ) : (
+  return (
     <Suspense fallback={<Loader />}>
       <Outlet />
       <Toaster position="top-right" reverseOrder={false} />
