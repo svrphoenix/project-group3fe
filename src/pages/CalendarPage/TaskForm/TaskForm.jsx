2b3645313bc124f1094b2c361e41ea5bfeb1c564
Crash-afftar <sashakhymenko88@gmail.com>
2023-08-18 20:41:06 +0300

    1808

--- src/pages/CalendarPage/TaskForm/TaskForm.jsx
+++ src/pages/CalendarPage/TaskForm/TaskForm.jsx
@@ -1,90 +1,100 @@
 import React from 'react';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { format } from 'date-fns';
+
 
 
 import {
   TaskFormStyled,
+  TaskContainer,
   TaskLabelStyled,
   TaskInputStyled,
-  Wrapper,
+  TimeWrapper,
+  RadioButtonContainer,
   RadioButtonGorup,
   RadioButtonLabel,
   RadioButtonInput,
+  ButtonContainer,
   AddButton,
   CancelBtn,
   ButtonIcon,
-  Errors,
 } from './TaskForm.styled';
 
 const TaskForm = () => {
   return (
     <TaskFormStyled>
-      <TaskLabelStyled htmlFor="title">
-        Title
-        <TaskInputStyled
-          type="text"
-          name="title"
-          id="title"
-          placeholder="Enter text"
-        />
-        <Errors>Error message</Errors>
-      </TaskLabelStyled>
-
-      <Wrapper>
-        <TaskLabelStyled htmlFor="start">
-          Start
+      <TaskContainer>
+        <TaskLabelStyled htmlFor="title">
+          Title
           <TaskInputStyled
-            type="time"
-            step="60"
-            name="start"
-            id="start"
-            placeholder="Select time"
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Enter text"
           />
-          <Errors>Error message</Errors>
         </TaskLabelStyled>
+      </TaskContainer>
 
-        <TaskLabelStyled htmlFor="end">
-          End
-          <TaskInputStyled
-            type="time"
-            step="60"
-            name="end"
-            id="end"
-            placeholder="Select time"
-          />
-          <Errors>Error message</Errors>
-        </TaskLabelStyled>
-      </Wrapper>
+      <TimeWrapper>
+        <TimeWrapper>
+          <TaskLabelStyled htmlFor="start">
+            Start
+            <TaskInputStyled
+              type="time"
+              step="60"
+              name="start"
+              id="start"
+              placeholder="Select time"
+            />
+          </TaskLabelStyled>
+        </TimeWrapper>
 
-      <RadioButtonGorup>
-        {['low', 'medium', 'high'].map(priority => (
-          <RadioButtonLabel key={priority}>
-            <RadioButtonInput type="radio" value={priority} name="priority" />
-            {priority}
+        <TimeWrapper>
+          <TaskLabelStyled htmlFor="end">
+            End
+            <TaskInputStyled
+              type="time"
+              step="60"
+              name="end"
+              id="end"
+              placeholder="Select time"
+            />
+          </TaskLabelStyled>
+        </TimeWrapper>
+      </TimeWrapper>
+
+      <RadioButtonContainer>
+        <RadioButtonGorup>
+          <RadioButtonLabel>
+            <RadioButtonInput type="radio" name="option" value="low" />
           </RadioButtonLabel>
-        ))}
-      </RadioButtonGorup>
+        </RadioButtonGorup>
+        <RadioButtonGorup>
+          <RadioButtonLabel>
+            <RadioButtonInput type="radio" name="option" value="medium" />
+          </RadioButtonLabel>
+        </RadioButtonGorup>
+        <RadioButtonGorup>
+          <RadioButtonLabel>
+            <RadioButtonInput type="radio" name="option" value="high" />
+          </RadioButtonLabel>
+        </RadioButtonGorup>
+      </RadioButtonContainer>
 
-      <Wrapper>
+      <ButtonContainer>
         <ButtonIcon>
           {/* <Pencil width="18" height="18" fill="none" stroke="#ffffff" /> */}
           Edit
         </ButtonIcon>
-        <AddButton
-          aria-label="Button add"
-          type="submit"
-          style={{ width: '50%' }}
-        >
+        <AddButton aria-label="Button add" type="submit">
           {/* <Plus width="20" height="20" fill="none" stroke="#ffffff" /> */}
           Add
         </AddButton>
-        <CancelBtn
-          aria-label="Button cancel"
-          type="button"
-          style={{ width: '50%' }}
-        >
+        <CancelBtn aria-label="Button cancel" type="button">
           Cancel
         </CancelBtn>
-      </Wrapper>
+      </ButtonContainer>
     </TaskFormStyled>
   );
 };
