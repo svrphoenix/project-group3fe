f037c5db9123ae7a3b1dfaf9682609c0bde028f6
Oleksander Novak <sandrix@ukr.net>
2023-08-19 00:16:37 +0300

    complited architecture ChoosedDay

--- src/components/ChoosedDay/ChoosedDay.jsx
+++ src/components/ChoosedDay/ChoosedDay.jsx
@@ -4,22 +4,61 @@ import { useEffect } from 'react';
 import * as tasksOperations from 'redux/tasks/operations';
 import { useParams } from 'react-router-dom';
 import { selectTasks } from 'redux/tasks/selectors';
-import { ColumnsTasksList } from './ColumnsTasksList/ColumnsTasksList';
 import { DayCalendarHead } from 'components/ChoosedDay/DayCalendarHead/DayCalendarHead';
+import { TasksColumnsList } from './TasksColumnsList/TasksColumnsList';
 
 export const ChoosedDay = () => {
   const dispatch = useDispatch();
-  const tasks = useSelector(selectTasks);
+  // const tasks = useSelector(selectTasks);
+  const tasks = [
+    {
+      title: 'Do a morning exercise Do a morning exercise',
+      start: '09:00',
+      end: '09:10',
+      priority: 'low',
+      date: '2023-08-18',
+      category: 'to-do',
+      owner: {
+        _id: 'tyd5eb99a3ed01b542412a78',
+        avatarURL:
+          'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/avatar-icon.png',
+      },
+    },
+    {
+      title: 'Buy milk',
+      start: '09:00',
+      end: '09:10',
+      priority: 'high',
+      date: '2023-08-18',
+      category: 'to-do',
+      owner: {
+        _id: 'tyd5eb99a3ed01b542412a87',
+        avatarURL: 'https://vrt.com/upload/avatar.jpg',
+      },
+    },
+    {
+      title: 'Do something bad',
+      start: '09:00',
+      end: '09:10',
+      priority: 'medium',
+      date: '2023-08-18',
+      category: 'in-progress',
+      owner: {
+        _id: 'tyd5eb99a3ed01b542412a79',
+        avatarURL: 'https://vrt.com/upload/avatar.jpg',
+      },
+    },
+  ];
   const { currentDay } = useParams();
 
   const daysTasks = {
-    'to-do': tasks.filter(
+    todo: tasks.filter(
       task => task.date === currentDay && task.category === 'to-do'
     ),
-    inProgresDaysTasks: tasks.filter(
+    inProgress: tasks.filter(
       task => task.date === currentDay && task.category === 'in-progress'
     ),
-    doneDaysTasks: tasks.filter(
+    done: tasks.filter(
       task => task.date === currentDay && task.category === 'done'
     ),
   };
@@ -37,7 +76,7 @@ export const ChoosedDay = () => {
   return (
     <>
       <DayCalendarHead dateObject={dateObject} />
-      <ColumnsTasksList currentDay={currentDay} daysTasks={daysTasks} />
+      <TasksColumnsList daysTasks={daysTasks} />
     </>
   );
 };
--- src/components/ChoosedDay/TasksColumn/TasksColumn.jsx
+++ src/components/ChoosedDay/TasksColumn/TasksColumn.jsx
@@ -1,187 +1,25 @@
 import useModalToggle from 'hooks/useModalToggle';
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { selectTasks } from 'redux/tasks/selectors';
+import { ColumnHeadBar } from '../ColumnHeadBar/ColumnHeadBar';
+import { ColumnTasksList } from '../ColumnTasksList/ColumnTasksList';
+import { AddTaskBtn } from '../AddTaskBtn/AddTaskBtn';
 import { Modal } from 'components/Modal/Modal';
-import { ReactComponent as PlusCircle } from 'images/icons/plus-circle.svg';
-import { ReactComponent as Remove } from 'images/icons/arrow-circle-broken-right.svg';
-import { ReactComponent as Pencil } from 'images/icons/pencil-task.svg';
-import { ReactComponent as Trash } from 'images/icons/trash-task.svg';
 import * as SC from './TasksColumn.styled';
 
-export const TasksColumn = ({ currentDay }) => {
+export const TasksColumn = ({ columnTitle, columnTasks }) => {
   const { showModal, onToggleModal } = useModalToggle();
-  const [category, setCategory] = useState(null);
-  const tasks = useSelector(selectTasks);
-
-  const onSetCategory = e => {
-    setCategory(e.currentTarget.getAttribute('data-custom'));
-  };
-
-  const removeTask = () => {
-    console.log('editTask');
-  };
-
-  const editTask = () => {
-    console.log('editTask');
-  };
-  const deleteTask = () => {
-    console.log('deleteTask');
-  };
 
   return (
     <>
-      <SC.DaysTasksContainer onClick={onSetCategory} data-custom="to-do">
-        <SC.TasksHeader>
-          <SC.TasksTitle>To do</SC.TasksTitle>
-          <SC.IconBtn onClick={onToggleModal} aria-label="add task">
-            <PlusCircle />
-          </SC.IconBtn>
-        </SC.TasksHeader>
-        <SC.TasksBox>
-          {tasks
-            .filter(task => task.category === 'to-do')
-            .map(task => (
-              <SC.TaskItem key={task._id}>
-                <SC.TaskDescr>{task.title}</SC.TaskDescr>
-                <SC.TaskInfo>
-                  <SC.AvatarWrapper>
-                    <SC.Avatar />
-                    <SC.Priority
-                      style={{
-                        background:
-                          task.priority === 'High'
-                            ? '#EA3D65'
-                            : task.priority === 'Medium'
-                            ? '#F3B249'
-                            : task.priority === 'Low'
-                            ? '#72C2F8'
-                            : null,
-                      }}
-                    >
-                      {task.priority}
-                    </SC.Priority>
-                  </SC.AvatarWrapper>
-                  <SC.ControlPanel>
-                    <SC.IconBtn onClick={removeTask} aria-label="remove task">
-                      <Remove />
-                    </SC.IconBtn>
-                    <SC.IconBtn onClick={editTask} aria-label="edit task">
-                      <Pencil />
-                    </SC.IconBtn>
-                    <SC.IconBtn onClick={deleteTask} aria-label="delete task">
-                      <Trash />
-                    </SC.IconBtn>
-                  </SC.ControlPanel>
-                </SC.TaskInfo>
-              </SC.TaskItem>
-            ))}
-        </SC.TasksBox>
-        <SC.CreateTasksBtn onClick={onToggleModal}>Add task</SC.CreateTasksBtn>
-      </SC.DaysTasksContainer>
-      <SC.DaysTasksContainer onClick={onSetCategory} data-custom="in-progress">
-        <SC.TasksHeader>
-          <SC.TasksTitle>In progress</SC.TasksTitle>
-          <SC.IconBtn onClick={onToggleModal} aria-label="add task">
-            <PlusCircle />
-          </SC.IconBtn>
-        </SC.TasksHeader>
-        <SC.TasksBox>
-          {tasks
-            .filter(task => task.category === 'in-progress')
-            .map(task => (
-              <SC.TaskItem key={task._id}>
-                <SC.TaskDescr>{task.title}</SC.TaskDescr>
-                <SC.TaskInfo>
-                  <SC.AvatarWrapper>
-                    <SC.Avatar />
-                    <SC.Priority
-                      style={{
-                        background:
-                          task.priority === 'High'
-                            ? '#EA3D65'
-                            : task.priority === 'Medium'
-                            ? '#F3B249'
-                            : task.priority === 'Low'
-                            ? '#72C2F8'
-                            : null,
-                      }}
-                    >
-                      {task.priority}
-                    </SC.Priority>
-                  </SC.AvatarWrapper>
-                  <SC.ControlPanel>
-                    <SC.IconBtn onClick={removeTask} aria-label="remove task">
-                      <Remove />
-                    </SC.IconBtn>
-                    <SC.IconBtn onClick={editTask} aria-label="edit task">
-                      <Pencil />
-                    </SC.IconBtn>
-                    <SC.IconBtn onClick={deleteTask} aria-label="delete task">
-                      <Trash />
-                    </SC.IconBtn>
-                  </SC.ControlPanel>
-                </SC.TaskInfo>
-              </SC.TaskItem>
-            ))}
-        </SC.TasksBox>
-        <SC.CreateTasksBtn onClick={onToggleModal}>Add task</SC.CreateTasksBtn>
-      </SC.DaysTasksContainer>
-      <SC.DaysTasksContainer onClick={onSetCategory} data-custom="done">
-        <SC.TasksHeader>
-          <SC.TasksTitle>Done</SC.TasksTitle>
-          <SC.IconBtn onClick={onToggleModal} aria-label="add task">
-            <PlusCircle />
-          </SC.IconBtn>
-        </SC.TasksHeader>
-        <SC.TasksBox>
-          {tasks
-            .filter(task => task.category === 'done')
-            .map(task => (
-              <SC.TaskItem key={task._id}>
-                <SC.TaskDescr>{task.title}</SC.TaskDescr>
-                <SC.TaskInfo>
-                  <SC.AvatarWrapper>
-                    <SC.Avatar />
-                    <SC.Priority
-                      style={{
-                        background:
-                          task.priority === 'High'
-                            ? '#EA3D65'
-                            : task.priority === 'Medium'
-                            ? '#F3B249'
-                            : task.priority === 'Low'
-                            ? '#72C2F8'
-                            : null,
-                      }}
-                    >
-                      {task.priority}
-                    </SC.Priority>
-                  </SC.AvatarWrapper>
-                  <SC.ControlPanel>
-                    <SC.IconBtn onClick={removeTask} aria-label="remove task">
-                      <Remove />
-                    </SC.IconBtn>
-                    <SC.IconBtn onClick={editTask} aria-label="edit task">
-                      <Pencil />
-                    </SC.IconBtn>
-                    <SC.IconBtn onClick={deleteTask} aria-label="delete task">
-                      <Trash />
-                    </SC.IconBtn>
-                  </SC.ControlPanel>
-                </SC.TaskInfo>
-              </SC.TaskItem>
-            ))}
-        </SC.TasksBox>
-        <SC.CreateTasksBtn onClick={onToggleModal}>Add task</SC.CreateTasksBtn>
-      </SC.DaysTasksContainer>
-      {showModal && (
-        <Modal
-          category={category}
-          date={currentDay}
+      <SC.DaysTasksContainer>
+        <ColumnHeadBar
           onToggleModal={onToggleModal}
-        ></Modal>
-      )}
+          columnTitle={columnTitle}
+        />
+        <ColumnTasksList columnTasks={columnTasks} />
+        <AddTaskBtn onToggleModal={onToggleModal} />
+      </SC.DaysTasksContainer>
+
+      {showModal && <Modal onToggleModal={onToggleModal}></Modal>}
     </>
   );
 };
