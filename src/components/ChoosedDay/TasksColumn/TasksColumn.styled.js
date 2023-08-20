a230ca2b9c7466dd79cbe1b4fd538c99fa399e8c
Alexandr Katyshev <katyshev1@gmail.com>
2023-08-20 20:17:17 +0300

    TasksColumn theming done

--- src/components/ChoosedDay/TasksColumn/TasksColumn.styled.js
+++ src/components/ChoosedDay/TasksColumn/TasksColumn.styled.js
@@ -4,8 +4,8 @@ export const DaysTasksContainer = styled.div`
   width: 335px;
   padding: 20px;
   border-radius: 8px;
-  border: 1px solid rgba(220, 227, 229, 0.5);
-  background: #fff;
+  border: var(--taskitem-border);
+  background: var(--second-Bckg-Color);
   @media screen and (min-width: 768px) {
     width: 344px;
   }
