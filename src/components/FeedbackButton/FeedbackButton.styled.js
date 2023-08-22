de76458dbb9bbe59804e5b994f25e9fa5c6edbc3
Alexandr Katyshev <katyshev1@gmail.com>
2023-08-22 08:03:38 +0300

    fixed btn hover

--- src/components/FeedbackButton/FeedbackButton.styled.js
+++ src/components/FeedbackButton/FeedbackButton.styled.js
@@ -4,14 +4,14 @@ export const FeedbackButton = styled.button`
   padding: 8px 20px;
   border-radius: 10px;
   border: none;
-  background: #3e85f3;
+  background-color: #3e85f3;
   color: #fff;
   text-align: center;
   font-family: 'Inter SemiBold';
   font-size: 12px;
   font-weight: 600;
   line-height: 16px;
-  transition: all 0.3s;
+  transition: background-color var(--cubic);
   cursor: pointer;
   @media screen and (min-width: 768px) {
     padding: 12px 32px;
@@ -19,8 +19,8 @@ export const FeedbackButton = styled.button`
     font-size: 14px;
     line-height: 18px;
   }
-  &:hover {
-    box-shadow: 2px 2px 2px 1px rgba(62, 133, 243, 0.4);
-    transform: scale(1.05);
+  &:hover,
+  &:focus {
+    background-color: #2b78ef;
   }
 `;
